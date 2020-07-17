6000f8b41883a593871b0997e46976f2fa0dd22d
jchoi64 <64911884+jchoi64@users.noreply.github.com>
2020-07-16 18:43:07 -0600

    added initial GUI class

---
+++ Python_files/main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+class project_GUI():
+    def __init__(self,window):
+        window.title("592 Project")
+
+        window.mainloop()
+
+
+window = tk.Tk()
+gui_start = project_GUI(window)
