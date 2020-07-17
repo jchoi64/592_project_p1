5f419b80cf38dc187d5a2e374268f331701d892f
jchoi64 <64911884+jchoi64@users.noreply.github.com>
2020-07-17 16:56:33 -0600

    updated comments for mongodb_init

--- Python_files/database/mongodb_init.py
+++ Python_files/database/mongodb_init.py
@@ -1,21 +1,30 @@
+#import required libraries
 import pymongo
 from pymongo import MongoClient
 import pandas as pd
-import mongodb_userinfo
 import os
 
+#import connection string, hidden from git for security reasons
+import mongodb_userinfo
+
+#creates a collection in mongodb of the csv
 def insert_collection(file_name):
+    #collection name removes the .csv portion of the file name
     file_name_trimmed = file_name[:-4]
     collection = db[file_name_trimmed]
 
-
+    #organizes the csv into a dictionary
     file = pd.read_csv(file_name)
     result = file.to_dict(orient = 'records')
 
+    #sets the _id of each list element
+    #i.e the _id is the row number
     counter = 0
     for i in result:
         i["_id"] = counter
         counter += 1
+
+    #inserts the dictionary into mongodb
     try:
         collection.insert_many(result)
     except:
@@ -25,7 +34,7 @@ def insert_collection(file_name):
 cluster = MongoClient(mongodb_userinfo.connection_string())
 db = cluster["592_Project_1"]
 
-#make sure you are in csv directory file
+#make sure you are in the csv directory file
 files = os.listdir()
 
 for file in files:
