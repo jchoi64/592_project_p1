29f10f33509a1fcaaef48ce625258115654cc826
jchoi64 <64911884+jchoi64@users.noreply.github.com>
2020-07-16 19:36:33 -0600

    added comboboxes

--- Python_files/main.py
+++ Python_files/main.py
@@ -1,19 +1,50 @@
 import tkinter as tk
+from tkinter import ttk
 
 class project_GUI():
     def __init__(self,window):
         window.title("592 Project")
         window.geometry("500x500")
 
+        #frames
         frame_buttons = tk.Frame(
             master = window,
-            width = 30,
-            height = 500,
-            bg = "yellow"
+            # width = 30,
+            # height = 500,
+            bg = "yellow",
+            borderwidth = 15
         )
-        frame_display = tk.Frame()
+        # frame_buttons.grid(row = 1, column = 1, padx = 5, pady = 5)
+        frame_display = tk.Frame(
+            master = window
+        )
+        # frame_display.grid(row = 1, column = 2, padx = 5, pady = 5)
 
+        #items to be added to frame_buttons
         list_btn = []
+
+        #combo box
+        combo_traffic = ttk.Combobox(
+            master = frame_buttons,
+            values = [
+                "Accidents",
+                "Traffic volume"
+            ]
+        )
+        list_btn.append(combo_traffic)
+
+        combo_years = ttk.Combobox(
+            master = frame_buttons,
+            values = [
+                "2016",
+                "2017",
+                "2018"
+            ]
+        )
+        list_btn.append(combo_years)
+
+
+        #buttons
         list_btn_strings = ["Read","Sort","Analysis","Map"]
 
         for i in list_btn_strings:
@@ -22,9 +53,12 @@ class project_GUI():
                 master = frame_buttons
             ))
 
+        #status display    
+
         for i in list_btn:
             i.pack()
 
+        #items to be added to frame_display
         display = tk.Label(
             text = "Output will go here",
             master = frame_display
